55daeadd5a5d607791097091a7e4639c8f1f48c9
ryan-9138 <ryanandaya9138@gmail.com>
2023-12-05 16:42:46 +0000

    implement form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,7 @@
 """Forms of the project."""
-
+from django import forms
 # Create your forms here.
+class ThingsForm(forms.Form):
+    name = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
--- things/views.py
+++ things/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from.forms import ThingsForm
 
 def home(request):
+    form = ThingsForm()
     return render(request, 'home.html', {'form': form})
